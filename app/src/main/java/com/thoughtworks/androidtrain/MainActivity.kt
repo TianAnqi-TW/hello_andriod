58e19d8585a2f1a99265c8be3fff330dbcba2f60
anqi.tian <anqi.tian@thoughtworks.com>
2024-03-26 09:47:30 +0800

    add buttons in main activity

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val buttonContainer = findViewById<LinearLayout>(R.id.buttonContainer)
+
+        for (i in 1..10) {
+            val button = Button(this)
+            button.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            button.text = getString(R.string.button_text, i)
+            buttonContainer.addView(button)
         }
     }
 }
