701919ccd55f4adf32179347bdad9b2ca1a03633
anqi.tian <anqi.tian@thoughtworks.com>
2024-03-27 13:14:44 +0800

    add constraint activity class and logic

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.thoughtworks.androidtrain
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -26,7 +27,17 @@ class MainActivity : AppCompatActivity() {
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
             )
-            button.text = getString(R.string.button_text, i)
+            if (i == 1) {
+                button.text = "constraint_layout"
+                // 为按钮添加点击事件处理程序，启动 RelativeActivity
+                button.setOnClickListener {
+                    val intent = Intent(this, ConstraintActivity::class.java)
+                    startActivity(intent)
+                }
+            } else {
+                // 其他按钮显示默认的文本
+                button.text = getString(R.string.button_text, i)
+            }
             buttonContainer.addView(button)
         }
     }
