2932d718499f1e3c77181528bb33b482fef9aec6
anqi.tian <anqi.tian@thoughtworks.com>
2024-03-29 10:46:57 +0800

    add actionbar menu

--- app/src/main/java/com/thoughtworks/androidtrain/LanguageSelectionActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/LanguageSelectionActivity.kt
@@ -1,6 +1,8 @@
 package com.thoughtworks.androidtrain
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,22 +14,52 @@ class LanguageSelectionActivity : AppCompatActivity() {
 
         val btnAndroid = findViewById<Button>(R.id.btnAndroid)
         val btnJava = findViewById<Button>(R.id.btnJava)
+        // 获取 Toolbar 控件
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+
+        // 设置 ActionBar
+        setSupportActionBar(toolbar)
 
         // 设置 Android 按钮点击事件
         btnAndroid.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentLayout, AndroidFragment())
-                .commit()
+            showAndroidFragment()
         }
+
         // 设置 Java 按钮点击事件
         btnJava.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentLayout, JavaFragment())
-                .commit()
+            showJavaFragment()
         }
+
         // 默认显示 Android Fragment
+        showAndroidFragment()
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_language_selection, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_android -> {
+                showAndroidFragment()
+                true
+            }
+            R.id.menu_java -> {
+                showJavaFragment()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    private fun showAndroidFragment() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentLayout, AndroidFragment())
             .commit()
     }
+
+    private fun showJavaFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentLayout, JavaFragment())
+            .commit()
+    }
+
 }
