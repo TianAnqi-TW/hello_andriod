b6546242bf7fb042548cfa134d7ad542249f9ca2
anqi.tian <anqi.tian@thoughtworks.com>
2024-04-09 10:54:10 +0800

    refactor to add dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,18 +72,6 @@ android {
         jvmTarget = "1.8"
     }
 }
-// 定义依赖库的版本
-object Libs {
-    const val gson = "2.10.1"
-    const val coil = "2.6.0"
-    const val datastorePreferences = "1.0.0"
-    const val room = "2.4.0"
-    const val okhttp = "4.9.2"
-    const val retrofit = "2.9.0"
-    const val activityKtx = "1.4.0"
-    const val mockitoCore = "3.12.4"
-    const val mockitoKotlin = "3.2.0"
-}
 
 dependencies {
     implementation(libs.androidx.core.ktx)
@@ -91,19 +79,19 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("com.google.code.gson:gson:${Libs.gson}")
-    implementation("io.coil-kt:coil:${Libs.coil}")
-    implementation("androidx.datastore:datastore-preferences:${Libs.datastorePreferences}")
-    implementation("androidx.room:room-runtime:${Libs.room}")
-    implementation("com.squareup.okhttp3:okhttp:${Libs.okhttp}")
-    implementation ("com.squareup.retrofit2:retrofit:${Libs.retrofit}")
-    implementation ("com.squareup.retrofit2:converter-gson:${Libs.retrofit}")
-    implementation("androidx.room:room-ktx:${Libs.room}")
-    implementation("androidx.activity:activity-ktx:${Libs.activityKtx}")
-    kapt("androidx.room:room-compiler:${Libs.room}")
+    implementation(libs.gson)
+    implementation(libs.coil)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.okhttp)
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.activity.ktx)
+    kapt(libs.androidx.room.compiler)
     testImplementation(libs.junit)
-    testImplementation("org.mockito:mockito-core:${Libs.mockitoCore}")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:${Libs.mockitoKotlin}")
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
