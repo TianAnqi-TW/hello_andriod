7ac5e30de984e48d49c19ccca84b42ab987bcee3
anqi.tian <anqi.tian@thoughtworks.com>
2024-04-11 10:16:57 +0800

    add coli implementation to get tweet avatar

--- app/src/main/java/com/thoughtworks/androidtrain/tweet/ui/ComposeTweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/tweet/ui/ComposeTweetsActivity.kt
@@ -6,15 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,11 +32,13 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.annotation.ExperimentalCoilApi
 import com.thoughtworks.androidtrain.MyApplication
 import com.thoughtworks.androidtrain.R
 import com.thoughtworks.androidtrain.tweet.model.Tweet
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import coil.compose.rememberImagePainter
 
 class ComposeTweetsActivity : ComponentActivity() {
 
@@ -82,12 +84,20 @@ class ComposeTweetsActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalCoilApi::class)
     @Composable
     fun TweetItem(tweet: Tweet) {
         Row(modifier = Modifier.padding(16.dp)) {
             // 头像
+            val painter = rememberImagePainter(
+                data = tweet.sender?.avatar ?: "",
+                builder = {
+                    placeholder(R.drawable.avatar) // 默认头像资源
+                }
+            )
+            // 头像
             Image(
-                painter = painterResource(id = R.drawable.avatar),
+                painter = painter,
                 contentDescription = "Avatar",
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
