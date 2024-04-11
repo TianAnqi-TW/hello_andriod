9992c4d5caf9f3c75e047c8ebea70f61005e4324
anqi.tian <anqi.tian@thoughtworks.com>
2024-04-11 10:27:49 +0800

    add dialog to show avatar when click

--- app/src/main/java/com/thoughtworks/androidtrain/tweet/ui/ComposeTweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/tweet/ui/ComposeTweetsActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,19 +14,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -84,9 +89,9 @@ class ComposeTweetsActivity : ComponentActivity() {
         }
     }
 
-    @OptIn(ExperimentalCoilApi::class)
     @Composable
     fun TweetItem(tweet: Tweet) {
+        var showDialog by remember { mutableStateOf(false) }
         Row(modifier = Modifier.padding(16.dp)) {
             // 头像
             val painter = rememberImagePainter(
@@ -102,6 +107,9 @@ class ComposeTweetsActivity : ComponentActivity() {
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .size(48.dp)
+                    .clickable {
+                        showDialog = true
+                    }
             )
 
             // 创建一个空间，以便在图片和文本之间有一些间隔
@@ -127,6 +135,29 @@ class ComposeTweetsActivity : ComponentActivity() {
                     text = tweet.date?: "",
                     style = TextStyle(fontSize = 16.sp),
                     modifier = Modifier.padding(bottom = 8.dp)
+                )
+            }
+            // Dialog 展示大图
+            if (showDialog) {
+                AlertDialog(
+                    onDismissRequest = { showDialog = false },
+                    title = { Text(text = "头像大图") },
+                    confirmButton = {
+                        Button(
+                            onClick = { showDialog = false }
+                        ) {
+                            Text("关闭")
+                        }
+                    },
+                    modifier = Modifier.wrapContentSize(),
+                    text = {
+                        Image(
+                            painter = painter,
+                            contentDescription = "Avatar",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.size(200.dp)
+                        )
+                    }
                 )
             }
         }
